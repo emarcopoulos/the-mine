23342dcc776cd046b3e08a1db4318a3fcfbfc633
Elias Marcopoulos <emarcopoulos@gmail.com>
2016-07-19 16:37:01 -0500

    added a main file to start coding in

---
+++ playGame.java
@@ -0,0 +1,23 @@
+/*
+ * The Mine
+ *
+ * Developed by Elias Marcopoulos and Christa Spieth
+ *
+ */
+
+public class playGame {
+	public static void main(String[] args) {
+		//Steps:
+		//	1) login user
+		//	2) play game
+	}
+}
+
+/*
+		Scanner input = new Scanner(System.in);
+		System.out.print("New User? (y/n)");
+		String response = input.next();
+		if (response.equals("y")) {
+
+		}
+*/
